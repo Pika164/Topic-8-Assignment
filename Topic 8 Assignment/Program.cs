b74f1fe9f0d48787a8502cbdd485a0f6141a7fc1
Linkin Krysa <339742660@GoTVDSB.ca>
2025-03-26 14:34:22 -0400

    asfgyloasdf

--- Topic 8 Assignment/Program.cs
+++ Topic 8 Assignment/Program.cs
@@ -5,14 +5,29 @@
         static void Main(string[] args)
         {
             //Programing assignment task 1 - Reading in colors
-            List<String> color = new List<String>();
+            List<String> colors = new List<String>();
             Random generator = new Random();
-           
+
             for (int i = 0; i < 5; i++)
             {
                 Console.Write("Please enter a color: ");
+                colors.Add(Console.ReadLine());
             }
+
+            Console.WriteLine();
+
+            Console.WriteLine(colors[generator.Next(colors.Count)]);
+
+            Console.WriteLine();
+
+            Console.WriteLine(string.Join(", ", colors));
+
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to continue");
             Console.ReadLine();
+            Console.Clear();
+
+            //Programing assignment task 2 - Random Numbers
         }
     }
 }
