2d8c65c3c7e8b3305c1adb10e692ebef1c2aa0e2
Linkin Krysa <339742660@GoTVDSB.ca>
2025-03-31 14:44:55 -0400

    fix thingy and finish thingy

--- Topic 8 Assignment/Program.cs
+++ Topic 8 Assignment/Program.cs
@@ -29,8 +29,9 @@
 
             //Programing assignment task 2 - Random Numbers
             List<int> numbers = new List<int>();
-            int numAmount, maxValue, minValue;
+            int numAmount, maxValue, minValue, number, number2, count;
 
+            count = 0;
 
             Console.Write("How many numbers do you need?: ");
             numAmount = Convert.ToInt32(Console.ReadLine());
@@ -41,12 +42,35 @@
             Console.Write("Please eneter a Minimum value: ");
             minValue = Convert.ToInt32(Console.ReadLine());
 
+            Console.Clear();
+
             for (int i = 0; i < numAmount; i++)
             {
                 numbers.Add(generator.Next(minValue, maxValue + 1));
             }
             Console.WriteLine(string.Join(", ", numbers));
-
+            Console.WriteLine();
+            Console.Write("Enter a number from the list shown above: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            numbers.Remove(number);
+            Console.WriteLine();
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.Clear();
+            numbers.Add(number);
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine();
+            Console.Write("Enter a number from the list: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == number2)
+                {
+                    numbers.RemoveAll(numbers2);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.ReadLine();
         }
     }
 }
