bfa6b62130fba6d6c81f4cbfdcafd8d4607fdd76
Linkin Krysa <339742660@GoTVDSB.ca>
2025-04-01 14:19:04 -0400

    finised for the most part.

--- Topic 8 Assignment/Program.cs
+++ Topic 8 Assignment/Program.cs
@@ -4,35 +4,35 @@
     {
         static void Main(string[] args)
         {
-            ////Programing assignment task 1 - Reading in colors
-            //List<String> colors = new List<String>();
+            //Programing assignment task 1 - Reading in colors
+            List<String> colors = new List<String>();
             Random generator = new Random();
 
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.Write("Please enter a color: ");
-            //    colors.Add(Console.ReadLine());
-            //}
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Please enter a color: ");
+                colors.Add(Console.ReadLine());
+            }
 
-            //Console.WriteLine();
+            Console.WriteLine();
 
-            //Console.WriteLine(colors[generator.Next(colors.Count)]);
+            Console.WriteLine(colors[generator.Next(colors.Count)]);
 
-            //Console.WriteLine();
+            Console.WriteLine();
 
-            //Console.WriteLine(string.Join(", ", colors));
+            Console.WriteLine(string.Join(", ", colors));
 
-            //Console.WriteLine();
-            //Console.WriteLine("Press ENTER to continue");
-            //Console.ReadLine();
-            //Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+            Console.Clear();
 
             //Programing assignment task 2 - Random Numbers
             List<int> numbers = new List<int>();
             int numAmount, maxValue, minValue, number, number2, count;
 
             count = 0;
-
+            //Intro to assignment 2
             Console.Write("How many numbers do you need?: ");
             numAmount = Convert.ToInt32(Console.ReadLine());
 
@@ -44,6 +44,7 @@
 
             Console.Clear();
 
+            //Step 1 and 2 Below
             for (int i = 0; i < numAmount; i++)
             {
                 numbers.Add(generator.Next(minValue, maxValue + 1));
@@ -56,6 +57,8 @@
             Console.WriteLine();
             Console.WriteLine(string.Join(", ", numbers));
             Console.Clear();
+
+            //Step 3 and 4 Below
             numbers.Add(number);
             Console.WriteLine(string.Join(", ", numbers));
             Console.WriteLine();
@@ -65,10 +68,33 @@
             {
                 if (numbers[i] == number2)
                 {
-                    numbers.RemoveAll(numbers2);
+                    numbers[i]=0;
                 }
             }
             Console.WriteLine();
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+            Console.Clear();
+
+            //Step 5 and 6 Below
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i] = 0;
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+            Console.Clear();
+
+            //Step 7 and 8 below
+            numbers.Clear();
+            for (int i = 0; i < numAmount; i++)
+            {
+                numbers.Add(generator.Next(minValue, maxValue + 1));
+            }
             Console.WriteLine(string.Join(", ", numbers));
             Console.ReadLine();
         }
